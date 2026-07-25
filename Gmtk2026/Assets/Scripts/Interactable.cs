3b76a848b1c65ac675532d48ed29b58477f1d3f7
d.lupon <d.lupon@student.isartdigital.com>
2026-07-25 11:38:12 +0200

    Added Drop

--- Gmtk2026/Assets/Scripts/Interactable.cs
+++ Gmtk2026/Assets/Scripts/Interactable.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 public interface I_Interactable
 {
     public bool IsInteractable { get; set; }
-    public void Interact();
+    public void Interact(I_Interactor interactor = null);
     public bool CanInteract();
 }
