10ea90a33d4682466cfd95a5bc8b85ff3c5ff51d
d.lupon <d.lupon@student.isartdigital.com>
2026-07-26 17:51:20 +0200

    fix shop

--- Gmtk2026/Assets/Scripts/ShopStand.cs
+++ Gmtk2026/Assets/Scripts/ShopStand.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnBocal.Events;
 using UnityEngine;
 using UnityEngine.Events;
@@ -11,34 +12,46 @@ public class ShopStand : MonoBehaviour, I_Interactable, I_BulletOrRaycastTarget
     public delegate bool OnTrySpendMoneyHandler(int amount);
     public static event OnTrySpendMoneyHandler OnTrySpendMoney;
 
+    // -------~~~~~~~~~~================# // Interaction
+    bool I_Interactable.IsInteractable { get; set; } = true;
+
+    // -------~~~~~~~~~~================# // Component
+    [SerializeField] TextMeshProUGUI _priceRenderer;
 
     // -------~~~~~~~~~~================# // Upgrade
     [SerializeField] SOUpgrade _uprade;
+    SOUpgrade.Upgrade _current;
     bool _lock;
     int _count = 0;
 
-    #region I_Interactable implementation
-    bool I_Interactable.IsInteractable { get; set; } = true;
+    private void Start()
+    {
+        _current = _uprade[_count];
+        UpdateDisplay();
+    }
+
 
     public void Interact(I_Interactor interactor = null)
     {
         if (!CanInteract()) return;
 
-        SOUpgrade.Upgrade u = _uprade[_count];
+        _current = _uprade[_count];
 
         // If No More Upgrade Then Lock
-        if (u == null)
+        if (_current == null)
         {
             Lock();
             return;
         }
 
         // If the player has enough money, spend it and add time to the timer
-        if (!(OnTrySpendMoney != null && OnTrySpendMoney.Invoke(Mathf.RoundToInt(u.Price.Value)))) return;
+        if (!(OnTrySpendMoney != null && OnTrySpendMoney.Invoke(Mathf.RoundToInt(_current.Price.Value)))) return;
         
         // Buy Upgrade
-        EventBus<SOUpgrade.Upgrade>.Invoke(EventGame.Upgrade, u);
-        _count++;
+        EventBus<SOUpgrade.Upgrade>.Invoke(EventGame.Upgrade, _current);
+        
+        _current = _uprade[++_count];
+        UpdateDisplay();
 
         if (_uprade.CountMax < _count) Lock();
     }
@@ -64,7 +77,13 @@ public class ShopStand : MonoBehaviour, I_Interactable, I_BulletOrRaycastTarget
     {
         Interact();
     }
-    #endregion
+
+    public void UpdateDisplay()
+    {
+        if (_current == null || !_priceRenderer) return;
+
+        _priceRenderer.text = $"${Mathf.Round(_current.Price.Value)}";
+    }
 
     public static UnityEvent<float> OnAddMaxTime = new UnityEvent<float>();
  
